c71770df431a818854929af639044aee6e8c5f1a
Momojunzi <mark.holthaus@gmail.com>
2017-08-31 21:39:02 -0700

    info api works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,8 +15,20 @@ var app = {
 
 	//call functions that need to be called when the page loads in the start app method
 	startApp: function(){
-
+		this.bandSummary();
 	},
+	// ajax call to api for band summmary information
+	bandSummary: function(){
+		var artist = "Glass+Animals"
+
+		$.ajax({
+			url: "http://api.musicgraph.com/api/v2/artist/search?api_key=c8303e90962e3a5ebd5a1f260a69b138&name=" + artist,
+			method: "GET"
+		}).done(function(response){
+			var data = response;
+			console.log(data, artist);
+		});
+	}
 
 }
 
