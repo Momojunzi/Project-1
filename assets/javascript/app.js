3d4e5da80be27f946f9c16217166b485b2278148
Momojunzi <mark.holthaus@gmail.com>
2017-09-02 12:21:28 -0700

    search works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,6 +13,7 @@ var database = firebase.database();
 
 var app = {
 	artist: "Glass+Animals",
+	formattedArtist: "",
 	spotify: "",
 	youTube: "", 
 	bio: "",
@@ -21,10 +22,10 @@ var app = {
 	startApp: function(){
 		this.callMusicGraph();
 		this.callLastFm();
+		this.searchBand();
 	},
 	// ajax call to api for band summmary information
 	callMusicGraph: function(){
-		
 		var musicGraphId;
 		//get general music graph info like music graph id and spotify and youtube ids
 		$.ajax({
@@ -50,15 +51,32 @@ var app = {
 		});
 	},
 	callLastFm: function(){
+		// call lastFm for img
 		$.ajax({
 			url: "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + this.artist + "&api_key=651401dc542766eb3d39ccee850cb749&format=json",
 			method: "GET"
 		}).done(function(response){
 			console.log(typeof(response.artist.image[3]['#text']));
 			this.imageUrl = response.artist.image[3]['#text'];
-			$("#image-div").append('<img class="img-responsive" src=' + this.imageUrl + '>');
+			$("#image-div").html('<img class="img-responsive" src=' + this.imageUrl + '>');
 		});
-		
+	},
+	searchBand: function() {
+		// click search button
+		$('#search-button').on('click', function(){
+			event.preventDefault();
+			console.log('hi');
+			// get value from search input
+			var searchedArtist = $('#search-input').val().trim();
+			console.log(searchedArtist);
+			app.artist = searchedArtist;
+			// format artist name for query string
+			app.formattedArtist = searchedArtist.split(" ").join('+');
+			// recall music graph and last fm to grab artist info
+			app.callMusicGraph();
+			app.callLastFm();
+			console.log(app.formattedArtist);
+		});
 		
 	}
 
